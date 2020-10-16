7e6252e968c3dc396f381f3fff794ffc9040088a
Renee Desporte <desporterenee@gmail.com>
2020-10-16 11:27:02 -0500

    The Perceptron class and Iris Classification training are complete.

--- IrisClassification.py
+++ IrisClassification.py
@@ -1,3 +1,41 @@
 import pandas as pd
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-# df.tail()     <- I used Jupyter to get the output of this line...
+# df.tail()     <- I used Jupyter to get the output of this, which just confirms that we got the right data.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#grabs the first 100 class labels corresponding to setosa and versicolor.
+y = df.iloc[0:100, 4].values
+
+#coverts class labels to -1 (for setosa) and 1 (for the rest, i.e., versicolor).
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+#grabs sepal length and petal length, two features in columns 0 and 2.
+X = df.iloc[0:100, [0,2]].values
+
+#plots the the two classes on a graph with sepal length as one axis and petal length as the other.
+plt.scatter(X[:50, 0], X[:50, 1], color='green', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='purple', marker='x', label='versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.show()
+
+from Perceptron import *
+
+#creates perceptron using the class we made
+ppn = Perceptron(eta=0.1, n_iter=10)
+
+#trains the perceptron on our Iris data
+ppn.fit(X, y)
+
+#plots the error for each epoch
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
+plt.show()
+
+"""
+The perceptron converges after 6 epochs.
+There is a final step to plot the decision boundary, which I have omitted.
+"""
--- Perceptron.py
+++ Perceptron.py
@@ -21,7 +21,7 @@ class Perceptron(object):
     """
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
-        self.n_inter = n_inter
+        self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, y):
